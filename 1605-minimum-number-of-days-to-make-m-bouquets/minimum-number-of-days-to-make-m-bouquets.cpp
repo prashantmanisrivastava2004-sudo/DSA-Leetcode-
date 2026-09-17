46fc18a3dcedd0319ffaf85c98d5c65cf4a231fd
Prashant_Mani <prashantmanisrivastava2004@gmail.com>
2026-09-17 18:21:13 +0530

    Time: 25 ms (35.28%) | Memory: 70.4 MB (80.41%) - LeetSync

---
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    int maximum_ele(vector<int>& bloomday){
+        int mx = INT_MIN;
+        for(int i=0; i<bloomday.size(); i++){
+            mx = max(mx, bloomday[i]);
+        }
+        return mx;
+    }
+
+    int minimum_ele(vector<int>& bloomday){
+        int mn = INT_MAX;
+        for(int i=0; i<bloomday.size(); i++){
+            mn = min(mn, bloomday[i]);
+        }
+        return mn;
+    }
+
+    bool ispossible(vector<int>& bloomday,int el, int m, int k){
+        int count=0;
+        int bouquet=0;
+        for(int i=0; i<bloomday.size(); i++){
+            if((bloomday[i]) <= el){
+                count++;
+                if(count % k==0){
+                    bouquet++;
+                    count=0;
+                }
+            }else{
+                count=0;
+            }
+        }
+        if(bouquet>=m){
+            return true;
+        }
+        return false;
+    }
+    int minDays(vector<int>& bloomday, int m, int k) {
+        if((long long)m * k > bloomday.size()){
+        return -1;
+        }
+        int st = minimum_ele(bloomday);
+        int end = maximum_ele(bloomday);
+        int result=0;
+        while(st <= end){
+            int mid = st+(end-st)/2;
+
+            if(ispossible(bloomday,mid,m,k)){
+                result = mid;
+                end = mid-1;
+            }else{
+                st =  mid+1;
+            }
+        }
+        return result;
+    }
+};
